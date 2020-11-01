d160405e58904177a4bea7948c96d3a4b6aa8378
Henok <hgebeyehu4486@gmail.com>
2020-11-01 12:52:02 -0500

    Aesthetic changes to file path

--- Study-Enator/main.py
+++ Study-Enator/main.py
@@ -5,19 +5,19 @@ import numpy as np
 import cv2
 
 path_to_file = 'C:/Users/Henok/source/repos/Study-Enator/Study-Enator/Accessories/'
-prototxt = 'deploy.prototxt'
-model = 'res10_300x300_ssd_iter_140000_fp16.caffemodel'
-weights = 'opencv_face_detector_uint8.pb'
-image = 'elon.png'
+prototxt = path_to_file + 'deploy.prototxt'
+model = path_to_file + 'res10_300x300_ssd_iter_140000_fp16.caffemodel'
+weights = path_to_file + 'opencv_face_detector_uint8.pb'
+image = path_to_file + 'elon.png'
 min_confidence = .5
 
 #load our serialized model from disk
-net = cv2.dnn.readNetFromCaffe(path_to_file+prototxt, path_to_file+model)  
+net = cv2.dnn.readNetFromCaffe(prototxt, model)  
 
 # load the input image and construct an input blob for the image
 # by resizing to a fixed 300x300 pixels and then normalizing it.
 
-image = cv2.imread(path_to_file+image)
+image = cv2.imread(image)
 (h,w) =  image.shape[:2]
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
         (300,300),(104.0,177.0, 123.0))
