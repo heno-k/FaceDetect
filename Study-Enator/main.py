e3d377723993f39142bc2abe3b3c310a27601639
Henok <hgebeyehu4486@gmail.com>
2020-11-08 11:15:55 -0500

    Able to detect face from video stream now

--- Study-Enator/main.py
+++ Study-Enator/main.py
@@ -4,52 +4,76 @@ from CameraInterface.PiCamUtil import *
 import numpy as np
 import cv2
 
+import imutils
+import time
+from imutils.video import VideoStream
+
+Input_type = ImSrc.VideoStream
+
 path_to_file = 'C:/Users/Henok/source/repos/Study-Enator/Study-Enator/Accessories/'
-prototxt = path_to_file + 'deploy.prototxt'
-model = path_to_file + 'res10_300x300_ssd_iter_140000_fp16.caffemodel'
-weights = path_to_file + 'opencv_face_detector_uint8.pb'
-image = path_to_file + 'elon.png'
+prototxt_path = path_to_file + 'deploy.prototxt'
+model_path = path_to_file + 'res10_300x300_ssd_iter_140000_fp16.caffemodel'
+weights_path = path_to_file + 'opencv_face_detector_uint8.pb'
 min_confidence = .5
 
+if(Input_type == ImSrc.ImageFile):
+    image_path = path_to_file + 'elon.png'
+
 #load our serialized model from disk
-net = cv2.dnn.readNetFromCaffe(prototxt, model)  
+net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)  
 
-# load the input image and construct an input blob for the image
-# by resizing to a fixed 300x300 pixels and then normalizing it.
+#Generate the blob depending on the input types (ImageFile, VidFile, VidStream)
+if(Input_type == ImSrc.VideoStream):
+    #initialize the video stream and allow the camera sensor to warm up
+    print("[INFO] starting video stream...")
+    https_url = "http://raspberrypi:8080/?action=stream"
+    vs = VideoStream(https_url).start()
+    time.sleep(2.0)
 
-image = cv2.imread(image)
-(h,w) =  image.shape[:2]
-blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
-        (300,300),(104.0,177.0, 123.0))
+    # loop over the frames from the video stream
+    while True:
+        #grab the frame from the threaded video stream and resize it
+        # to have a max width of 400 pixels
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
 
-# pass the blob thorugh the network and obtain the detections and predictions
-print("[INFO] computing object detections...")
-net.setInput(blob)
-detections = net.forward()
+        # grab the frame dimensions and convert it to a blob
+        (h,w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300,300)), 1.0,
+                                     (300,300),(104.0, 177.0, 123.0))
+        # pass the blob through the network and obtain the detections
+        # and predictions
+        net.setInput(blob)
+        detections = net.forward()
+        
+        # place box around face
+        BoxFace(detections, min_confidence, frame, h, w)
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-#loop over the detections
-for i in range(0, detections.shape[2]):
-    #extract the confidence (i.e., probability) aassociated with the 
-    #prediction
-    confidence = detections[0, 0, i, 2]
+        # if the 'q' key was pressed, break from the loop
+        if(key == ord("q")):
+            break
+    #do a bit of clean up
+    cv2.destroyAllWindows()
+    vs.stop()
 
-    #filter out weak detections by ensuring the 'confidence' is 
-    #greater than the minimum confidence
-    if confidence > min_confidence:
-        #compute the (x,y)-coordinates of the bounding box for the 
-        #object
-        box = detections[0,0,i,3:7] * np.array([w,h,w,h])
-        (startX, startY, endX, endY) = box.astype("int")
+elif(Input_type == ImSrc.ImageFile):
+    # load the input image and construct an input blob for the image
+    # by resizing to a fixed 300x300 pixels and then normalizing it.
+    image = cv2.imread(image_path)
+    (h,w) = image.shape[:2]
+    blob  = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
+            (300,300),(104.0,177.0, 123.0))
 
-        #draw the bouding box of the face along with the associated
-        #probability
-        text = "{:.2}%".format(confidence * 100)
-        y = startY - 10 if startY - 10 > 10 else startY + 10
-        cv2.rectangle(image, (startX,startY), (endX, endY), 
-                (0,0, 255),2)
-        cv2.putText(image, text, (startX, y),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0,0,255), 2)
+    # pass the blob thorugh the network and obtain the detections and predictions
+    print("[INFO] computing object detections...")
+    net.setInput(blob)
+    detections = net.forward()
 
-#show the output image
-cv2.imshow("Output", image)
-cv2.waitKey(0)
+    #place box around face
+    BoxFace(detections, min_confidence, frame, h, w)
+
+    #show the output image
+    cv2.imshow("Output", image)
+    cv2.waitKey(0)
